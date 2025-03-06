9be9236fc8eab1aeb65d7f061066416bdf4fdf2a
Alexander Kern <alexander.t.kern@gmail.com>
2025-03-06 11:45:07 -0600

    game loop basics

--- main.py
+++ main.py
@@ -2,9 +2,26 @@ import pygame
 from constants import *
 
 def main(): 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    # print("Starting Asteroids!")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    
+    while True: 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+
+        pygame.display.flip()
+        tick_val = clock.tick(60)
+        dt = tick_val / 1000
 
 
 
