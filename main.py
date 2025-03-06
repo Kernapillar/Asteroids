d9c602e4e904be7024b56916c255785fae5f0357
Alexander Kern <alexander.t.kern@gmail.com>
2025-03-06 12:02:12 -0600

    Add circleshape base class, player and draw in main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main(): 
     # print("Starting Asteroids!")
@@ -12,12 +13,15 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
     while True: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        player.draw(screen)
 
         pygame.display.flip()
         tick_val = clock.tick(60)
